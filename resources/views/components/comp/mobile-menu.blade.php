a5092a0bf1f3aa7679c5648c688f3cf6b7186e7f
Andrew <code70@inbox.ru>
2023-09-06 20:33:54 +0700

    mobile menu top

--- resources/views/components/comp/mobile-menu.blade.php
+++ resources/views/components/comp/mobile-menu.blade.php
@@ -1,16 +1,31 @@
-<div class="mobile-menu">
+<div class="mobile-menu show">
     <div class="container py-3">
         <div class="row">
-            <div class="col d-flex align-items-center justify-content-end">
-                <button class="close-menu" onclick="document.querySelector('.mobile-menu').classList.remove('show')"></button>
+            <div class="col-12 justify-content-between align-items-center d-flex pb-3">
+                <div class="phone-top">
+                    <a href="tel:{{ $company->phone }}">
+                        <h4>{{ $company->phone }}</h4>
+                    </a>
+                </div>
+                <button class="close-menu" onclick="document.querySelector('.mobile-menu').classList.remove('show')">
+                    <span></span>
+                    <span></span>
+                </button>
             </div>
-        </div>
-    </div>
-    <div class="container py-5">
-        <div class="row">
-            <div class="col">
-                <h4>{{ $company->name }}</h4>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor in dolores laudantium. Beatae illo facere sequi consectetur et suscipit deserunt laudantium eveniet, veniam aliquid? Deserunt nam doloribus error quae rerum.
+            <div class="header bg-dark text-white">
+                <div class="container py-3">
+                    <div class="row justify-content-between align-items-center">
+                        <div class="col d-block">
+                            <h4>{{ $company->name }}</h4>
+                            <span>{{ $company->city }}</span>
+                        </div>
+                        <div class="col d-flex justify-content-end">
+                            <div class="logo">
+                                <img src="http://via.placeholder.com/256x256" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
