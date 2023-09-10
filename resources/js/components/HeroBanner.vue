2821ad7b762aab1ec7c432f457db097574712172
Andrew <code70@inbox.ru>
2023-09-10 18:16:25 +0700

    update banner

--- resources/js/components/HeroBanner.vue
+++ resources/js/components/HeroBanner.vue
@@ -2,7 +2,13 @@
 export default{
     props:{
         company: Object
-    }
+    },data:()=>({
+        banners: [
+            {id: 1,image: 'http:via.placeholder.com/1920x1080', title: 'First title', description: 'First description'},
+            {id: 2,image: 'http:via.placeholder.com/1921x1081', title: 'Second title', description: 'Second description'}
+        ],
+        currentBanner: 1
+    })
 }
 </script>
 
@@ -11,10 +17,17 @@ export default{
         <div class="row">
             <div class="col-12">
                 <div class="hero-banner">
-                    <div class="overlay"></div>
-                    <div class="d-block position-relative text-white pb-4">
-                        <h2 class="title">{{ company.name }}</h2>
-                        <h6 class="description">{{ company.description }}</h6>
+                    <div class="hero-item" style="background: url('http://via.placeholder.com/1920x1080');" v-for="banner in banners" :key="banner" v-show="currentBanner == banner.id">
+                        <div class="overlay"></div>
+                        <div class="d-block position-relative text-white pb-4">
+                            <h2 class="title">{{ banner.title }}</h2>
+                            <h6 class="description">{{ banner.description }}</h6>
+                        </div>
+                    </div>
+                    <div class="pagination">
+                        <ul>
+                            <li v-for="item in banners" :key="banner" @click="currentBanner = item.id"></li>
+                        </ul>
                     </div>
                 </div>
             </div>
@@ -24,9 +37,42 @@ export default{
                 <a href="/catalog">
                     <div class="sub-banner">
                         <div class="overlay"></div>
-                        <div class="image" style="background: url('http://via.placeholder.com/512x512');"></div>
+                        <div class="image" style="background: url('/images/projects-banner.jpg');"></div>
                         <div class="text">
                             <h5>Проекты</h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-6 col-md-3 mb-3">
+                <a href="/slug_cat">
+                    <div class="sub-banner">
+                        <div class="overlay"></div>
+                        <div class="image" style="background: url('/images/slugs-banner.jpg');"></div>
+                        <div class="text">
+                            <h5>Услуги</h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-6 col-md-3 mb-3">
+                <a href="/portfoli">
+                    <div class="sub-banner">
+                        <div class="overlay"></div>
+                        <div class="image" style="background: url('/images/portfolio-banner.jpg');"></div>
+                        <div class="text">
+                            <h5>Портфолио</h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-6 col-md-3 mb-3">
+                <a href="/contact">
+                    <div class="sub-banner">
+                        <div class="overlay"></div>
+                        <div class="image" style="background: url('/images/contact-banner.jpg');"></div>
+                        <div class="text">
+                            <h5>Контакты</h5>
                         </div>
                     </div>
                 </a>
@@ -40,7 +86,7 @@ export default{
 // sub banner
 .sub-banner
     width: 100%
-    min-height: 160px
+    min-height: 140px
     border-radius: 10px
     overflow: hidden
     position: relative
@@ -84,18 +130,45 @@ export default{
     box-shadow: rgba(50, 50, 93, 0.35) 0px 13px 27px -5px, rgba(0, 0, 0, 0.4) 0px 8px 16px -8px
     min-height: 550px
     width: 100%
-    // background
-    background: url('https://s3.amazonaws.com/static-loghome/media/Fotolia_22487608_Subscription_Monthly_L_2018-09-13_17-13.jpg')
-    background-position: center !important
-    background-size: cover !important
-    background-repeat: no-repeat !important
     overflow: hidden
     border-radius: 10px
-    display: flex
-    align-items: end
-    justify-content: start
-    padding: 24px
     position: relative
+    & .pagination
+        position: absolute
+        left: calc( 50% - 25px )
+        bottom: 24px
+        width: 50px
+        display: flex
+        justify-content: center
+        align-items: center
+        & ul
+            display: flex
+            list-style: none
+            padding: 0
+            margin: 0
+            & li
+                cursor: pointer
+                display: block
+                margin: 0 5px
+                width: 10px
+                height: 10px
+                border-radius: 100%
+                background: #fff
+                &.active
+                    background: green
+    & .hero-item
+        background-position: center !important
+        background-size: cover !important
+        background-repeat: no-repeat !important
+        width: 100%
+        height: 100%
+        position: absolute
+        top: 0
+        left: 0
+        display: flex
+        align-items: end
+        justify-content: start
+        padding: 24px
     & .title
         font-size: 48px
         font-weight: 600
